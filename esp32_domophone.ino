d5c633271c61d4dab1a3ebf6857d55ddaa4118c9
Nikolay <rusatch@gmail.com>
2019-07-17 16:50:12 +0500

    added sketch for domophone

---
+++ esp32_domophone.ino
@@ -0,0 +1,113 @@
+#include <WiFi.h>
+#include "time.h"
+#include <RtcDS3231.h>
+#include <Wire.h>
+
+// Replace with your network credentials
+#define ssid      ""
+#define password  ""
+
+// NTP
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 14400;
+const int   daylightOffset_sec = 3600;
+#define hour4ntp 3
+#define min4ntp  0
+bool isHour4ntp = false;
+int currenthour, currentmin;
+
+RtcDS3231<TwoWire> rtcObject(Wire); // RTC
+
+#define dfRelay 33 // Pin for relay
+#define callDetect 17 // Pin for detect calling
+#define answerPin 16
+bool callState = false;
+
+void setup() {
+  pinMode(dfRelay, OUTPUT);
+  digitalWrite(dfRelay, HIGH); // Switch domophone line to optocoupler
+  pinMode(callDetect, INPUT);
+  pinMode(answerPin, OUTPUT);
+  digitalWrite(answerPin, LOW); // Wait for call
+
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+//  server.begin();
+
+  // RTC
+  rtcObject.Begin();
+  RtcDateTime currentTime = rtcObject.GetDateTime();
+  char rtcstr[40];
+  sprintf(rtcstr, "%d.%d.%d %d:%d:%d", currentTime.Year(), currentTime.Month(), currentTime.Day(), currentTime.Hour(), currentTime.Minute(), currentTime.Second());
+  Serial.print("Localtime: ");
+  Serial.println(rtcstr);
+
+  // NTP
+  execNtpUpdate();
+}
+
+void loop() {
+  // Detect calling
+  callState = digitalRead(callDetect);
+  if(callState){ // If calling
+    digitalWrite(answerPin, HIGH); // Answer the call
+    delay(1000);
+    digitalWrite(answerPin, LOW); // Reset the call
+  }
+  
+  // NTP update at 3:00
+  RtcDateTime currentTime = rtcObject.GetDateTime();
+  int currenthour = currentTime.Hour();
+  int currentmin = currentTime.Minute();
+  if(currenthour == hour4ntp & currentmin == min4ntp){
+    if(!isHour4ntp){
+      execNtpUpdate();
+      isHour4ntp = true;
+    }
+  } else {
+    isHour4ntp = false;
+  }
+
+  delay(10);
+}
+
+// NTP
+void execNtpUpdate(){
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  struct tm timeinfo;
+  for(int i=1; i <= 5; i++){
+    if(getLocalTime(&timeinfo)){
+      char timeStringBuff[40];
+      strftime(timeStringBuff, sizeof(timeStringBuff), "%Y.%m.%d %H:%M:%S", &timeinfo);
+      Serial.print("NTPdate: ");
+      Serial.println(timeStringBuff);
+      // Set NTP time to RTC module
+      int ntpyear = timeinfo.tm_year;
+      int ntpmon  = timeinfo.tm_mon;
+      int ntpday  = timeinfo.tm_mday;
+      int ntphour = timeinfo.tm_hour;
+      int ntpmin  = timeinfo.tm_min;
+      int ntpsec  = timeinfo.tm_sec;
+      RtcDateTime currentTime = RtcDateTime(ntpyear - 100,ntpmon + 1,ntpday,ntphour,ntpmin,ntpsec);
+      rtcObject.SetDateTime(currentTime);
+      Serial.println("Localtime updated");
+      break;
+    } else {
+      Serial.println("Failed to obtain time");
+      delay(1000);
+    }
+  }
+}
